ae4f0b6e74b2068e063b38ba4191eab12a45bfe3
Jason Goad <jgoad@calpoly.edu>
2014-04-03 13:05:57 -0700

    Added a comment line to the configureGit.sh file

--- configureGit.sh
+++ configureGit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script configures Git whenever you start working 
+# on multiple machines. Run it any time you decide to work with 
+# Git on a new computer
+
 git config --global user.name "Jason Goad"
 git config --global user.email "jgoad@calpoly.edu"
 git config --global color.ui "auto"
