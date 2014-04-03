fe437defc4ef3ce3467762385c9aa2865e145879
Jason Goad <jgoad@calpoly.edu>
2014-04-03 12:54:52 -0700

    adding config and pledge

---
+++ configureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "Jason Goad"
+git config --global user.email "jgoad@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
